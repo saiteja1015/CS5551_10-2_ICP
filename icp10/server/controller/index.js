5d09effd7a1c8d6144dc9047df1cd6322ac7a2c0
BinduMadamanchi <42659031+BinduMadamanchi@users.noreply.github.com>
2018-11-15 13:50:44 -0600

    ICP removed

--- icp10/server/controller/index.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-module.exports = function(app) {
-    var url = 'https://api.nutritionix.com/v1_1/search/?results=0:1&fields=*&appId=50cd56b2&appKey=0a2530b35c41daa7d46da44fc528208d&q=';
-    app.get('/api/rxlist', function(req, res) {
-        url = url+req.query.q;
-        request.get(url).pipe(res);
-        console.log(res);
-    });
-}
